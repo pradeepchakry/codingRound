5df5422dd30d8a911c3433d2f9450641f94225b5
Spiral Someone <pradeepchakry@gmail.com>
2019-05-27 22:37:38 +0530

    Locator fixes for FlghtBookingTest java

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -1,48 +1,69 @@
 import com.sun.javafx.PlatformUtil;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class FlightBookingTest {
 
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver;
+    private String originString;
+    private String destinationString;
+
+    @BeforeClass
+    public void setup() {
+        setDriverPath();
+        driver = new ChromeDriver();
+    }
 
 
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
 
-        setDriverPath();
+        originString = "Bangalore";
+        destinationString = "Delhi";
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
         driver.findElement(By.id("OneWay")).click();
 
         driver.findElement(By.id("FromTag")).clear();
-        driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
+        driver.findElement(By.id("FromTag")).sendKeys(originString);
+
 
         //wait for the auto complete options to appear for the origin
 
-        waitFor(2000);
+        waitFor(6000);
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
-        originOptions.get(0).click();
+        WebElement originOption = getNameBySuggestion(originOptions, originString);
+        originOption.click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys(destinationString);
+
 
         //wait for the auto complete options to appear for the destination
 
-        waitFor(2000);
+        waitFor(6000);
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
-        destinationOptions.get(0).click();
 
-        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+        WebElement destinationOption = getNameBySuggestion(destinationOptions, destinationString);
+        destinationOption.click();
+
+        driver.findElement(By.xpath("//*[@class='calendarIcon']/i")).click();
+
+        waitFor(3000);
+
+        driver.findElement(By.xpath("//td[contains(@class, 'selected')]")).click();
+
+        //*[@id="ui-datepicker-div"]/div[1]/table/tbody/tr[2]/td[4]/a
 
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
@@ -50,10 +71,6 @@ public class FlightBookingTest {
         waitFor(5000);
         //verify that result appears for the provided journey search
         Assert.assertTrue(isElementPresent(By.className("searchSummary")));
-
-        //close the browser
-        driver.quit();
-
     }
 
 
@@ -80,10 +97,32 @@ public class FlightBookingTest {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
         if (PlatformUtil.isWindows()) {
+            System.out.println("windows...");
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         }
         if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
         }
+    }
+
+    public WebElement getNameBySuggestion(List<WebElement> elementList, String searchString) {
+        WebElement element = null;
+        Iterator<WebElement> iteratorObj = elementList.iterator();
+        while(iteratorObj.hasNext()) {
+            element = (WebElement) iteratorObj.next();
+            if(element != null) {
+                if(element.getText().contains(searchString)) {
+                    return element;
+                } else {
+                    System.out.println("Problem with data. No such element that starts with " + searchString + " in the list. Returning null.");
+                }
+            }
+        }
+        return element;
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.close();
     }
 }
